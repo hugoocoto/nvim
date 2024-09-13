ae9e08c7061f1e0f1d827683765ee6f1fbac6dbe
Hugo Coto <hugocoto100305@gmail.com>
2024-09-13 23:52:59 +0200

    adding changes

--- install.sh
+++ install.sh
@@ -25,14 +25,22 @@ if git clone --depth 1 https://github.com/wbthomason/packer.nvim\
     echo "[+] Installed"
 else
     echo "[-] Error installing packer">&2
-    exit 1
 fi
 
 
 # Copiar la nueva carpeta
 echo "[+] Copying $NEW_NVIM_FOLDER to $CONFIG_NVIM"
-if mv "$NEW_NVIM_FOLDER" "$CONFIG_NVIM"; then
+if cp -r "$NEW_NVIM_FOLDER" "$CONFIG_NVIM" ; then
     echo "[+] Config moved"
+else
+    echo "[-] Error moving $NEW_NVIM_FOLDER to $CONFIG_NVIM" >&2
+    exit 1
+fi
+
+# Copiar la nueva carpeta
+echo "[+] Removing after folder"
+if rm -fr "$CONFIG_NVIM/after" ; then
+    echo "[+] Removed "
 else
     echo "[-] Error moving $NEW_NVIM_FOLDER to $CONFIG_NVIM" >&2
     exit 1
@@ -42,28 +50,36 @@ fi
 echo "[+] Installing packages"
 
 echo "    Source packer config        [--      ]"
-if ! nvim --headless +"so $NVIM_PACKER" +q; then
+if ! nvim --headless +"so" $NVIM_PACKER +q; then
     echo "[-] Error sourcing packer config" >&2
     exit 1
 fi
 
 echo "    Installing packages         [----    ]"
-if ! nvim --headless +"PackerInstall" +q; then
+if ! nvim --headless +"PackerInstall" $NVIM_PACKER +q; then
     echo "[-] Error installing packages" >&2
     exit 1
 fi
 
 echo "    Installing packages (check) [------  ]"
-if ! nvim --headless +"PackerInstall" +q; then
+if ! nvim --headless +"PackerInstall" $NVIM_PACKER +q; then
     echo "[-] Error on package installation check" >&2
     exit 1
 fi
 
 echo "    Compiling packer            [--------]"
-if ! nvim --headless +"PackerCompile" +q; then
+if ! nvim --headless +"PackerCompile" $NVIM_PACKER +q; then
     echo "[-] Error compiling packer" >&2
     exit 1
 fi
 
 echo "[+] Packages installed and compiled successfully."
+
+echo "[+] Copying after folder"
+if cp -r "$NEW_NVIM_FOLDER/after" "$CONFIG_NVIM/after" ; then
+    echo "[+] Copied"
+else
+    echo "[-] Error moving $NEW_NVIM_FOLDER to $CONFIG_NVIM" >&2
+    exit 1
+fi
 
