b2646676b9db68d54de86adf8c672ae1d32050d3
Hugo Coto Florez <hugocoto100305@gmail.com>
2025-04-10 17:19:32 +0200

    Trying to fix clangd formatting and failed

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -1,12 +1,11 @@
 local lsp = require("lsp-zero")
 
 
-require("lspconfig").clangd.setup({
-        cmd = {
-                "clangd",
-                "--offset-encoding=utf-16",
-        },
-})
+-- require("lspconfig").clangd.setup({
+--         cmd = {
+--                 "clangd",
+--         },
+-- })
 
 lsp.preset("recommended")
 
@@ -52,14 +51,13 @@ lsp.on_attach(function(client, buf)
         vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
         vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
         vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
-        vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
-        vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
+        vim.keymap.set("n", "[d", function() vim.diagnostic.next() end, opts)
+        vim.keymap.set("n", "]d", function() vim.diagnostic.prev() end, opts)
         vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
         vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
         vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
         vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
 end)
-
 
 lsp.setup()
 
