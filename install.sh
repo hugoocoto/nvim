382168ce39e489ab4a7b38011f66e578ec78c64a
Hugo Coto Florez <hugocoto100305@gmail.com>
2025-07-21 18:29:16 +0200

    fix script error

--- install.sh
+++ install.sh
@@ -5,10 +5,13 @@ SCRIPT_DIR="$( cd "$( dirname "$BASH_SOURCE[0]" )" && pwd )"
 nvim_config_dir=~/.config/nvim
 nvim_backup_dir=~/.config/nvim_old
 
+
 if [ -d "$nvim_config_dir" ]; then
         mkdir -p $nvim_backup_dir
         new_backup_dir=$nvim_backup_dir/`date --iso-8601=seconds`
         mv $nvim_config_dir $new_backup_dir
+else
+        mkdir -p $nvim_config_dir
 fi
 
 if [ "$1" == "-l" ]; then
