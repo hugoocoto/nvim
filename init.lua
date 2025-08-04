306c326f623ec5f2ad99cb9796906daec6f7aaca
Hugo Coto Florez <hugocoto100305@gmail.com>
2025-08-04 15:01:02 +0200

    removing like 50 lines

--- init.lua
+++ init.lua
@@ -22,6 +22,7 @@ vim.g.netrw_browse_split = 0
 vim.g.netrw_banner = 0
 vim.g.netrw_winsize = 25
 
+vim.opt.winborder = "rounded"
 vim.opt.guicursor = ""
 vim.opt.mouse = ""
 vim.opt.nu = true
@@ -58,25 +59,10 @@ vim.api.nvim_create_autocmd("BufReadPre", {
 -- Setup lazy.nvim
 require("lazy").setup({
         spec = {
-                --   -- add your plugins here
-
                 { "folke/lazy.nvim" },
 
                 -- Telescope
                 { "nvim-telescope/telescope.nvim", dependencies = { "nvim-lua/plenary.nvim" } },
-
-                -- Treesitter
-                {
-                        "nvim-treesitter/nvim-treesitter",
-                        build = function()
-                                local ts_update = require('nvim-treesitter.install').update({ with_sync = true })
-                                ts_update()
-                        end
-                },
-
-                -- Treesitter extras
-                { "nvim-treesitter/playground" },
-                { "nvim-treesitter/nvim-treesitter-context" },
 
                 -- Wakatime
                 { "wakatime/vim-wakatime" },
@@ -92,76 +78,28 @@ require("lazy").setup({
                         priority = 1000,
                 },
 
-                -- {
-                --         dir = "~/code/shoebill/",
-                --         name = "shoebill",
-                --         lazy = false,
-                --         priority = 1000,
-                -- },
-
                 {
                         'saghen/blink.cmp',
                         dependencies = { 'rafamadriz/friendly-snippets' },
                         version = '1.*',
                         opts = {
-                                -- 'default' (recommended) for mappings similar to built-in completions (C-y to accept)
-                                -- 'super-tab' for mappings similar to vscode (tab to accept)
-                                -- 'enter' for enter to accept
-                                -- 'none' for no mappings
-                                --
-                                -- All presets have the following mappings:
-                                -- C-space: Open menu or open docs if already open
-                                -- C-n/C-p or Up/Down: Select next/previous item
-                                -- C-e: Hide menu
-                                -- C-k: Toggle signature help (if signature.enabled = true)
-                                --
-                                -- See :h blink-cmp-config-keymap for defining your own keymap
                                 keymap = { preset = 'enter' },
-
-                                appearance = {
-                                        -- 'mono' (default) for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
-                                        -- Adjusts spacing to ensure icons are aligned
-                                        nerd_font_variant = 'mono'
-                                },
-
-                                -- (Default) Only show the documentation popup when manually triggered
+                                appearance = { nerd_font_variant = 'mono' },
                                 completion = { documentation = { auto_show = false } },
+                                sources = { default = { 'lsp', 'path', 'snippets', 'buffer' }, },
 
-                                -- Default list of enabled providers defined so that you can extend it
-                                -- elsewhere in your config, without redefining it, due to `opts_extend`
-                                sources = {
-                                        default = { 'lsp', 'path', 'snippets', 'buffer' },
-                                },
-
-                                -- (Default) Rust fuzzy matcher for typo resistance and significantly better performance
-                                -- You may use a lua implementation instead by using `implementation = "lua"` or fallback to the lua implementation,
-                                -- when the Rust fuzzy matcher is not available, by using `implementation = "prefer_rust"`
-                                --
-                                -- See the fuzzy documentation for more information
-                                -- fuzzy = { implementation = "prefer_rust_with_warning" }
                         },
                         opts_extend = { "sources.default" }
                 }
 
 
         },
-        -- -- Configure any other settings here. See the documentation for more details.
-        -- -- colorscheme that will be used when installing plugins.
-        -- install = { colorscheme = { "habamax" } },
-        -- automatically check for plugin updates
         checker = { enabled = true },
 })
 
-vim.cmd("colorscheme shoebill-dark")
+vim.cmd("colorscheme shoebill")
 
--- Evaluates an expression using python. It replaces the whole line
--- Example:
--- 1 + 2 * (2 - 3)
--- Would be replaced by
--- -1
-vim.keymap.set("v", "<leader>ev", "y:.!python3 -c \"print(<C-r>\")\" <CR>", { silent = true })
-
-vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+vim.keymap.set("n", "<leader>pv", ":Sex!<cr>")
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 vim.keymap.set("n", "J", "mzJ`z")
@@ -174,7 +112,6 @@ vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
 vim.keymap.set("n", "<leader>Y", [["+Y]])
 
 vim.keymap.set({ "n", "v" }, "<leader>d", [["_d]])
-vim.keymap.set("n", "Q", "<nop>")
 
 vim.keymap.set("n", "<leader><leader>", vim.lsp.buf.format)
 vim.keymap.set("n", "gd", vim.lsp.buf.definition)
@@ -196,19 +133,27 @@ vim.g.clipboard = {
 }
 
 
-vim.lsp.config('clangd', {
-        on_attach = function()
-                vim.keymap.set("n", "<leader><leader>", ":w<cr>:! clang-format -i %<cr><cr>",
-                        { silent = true, noremap = true })
-        end
-})
+-- vim.lsp.config('clangd', {
+--         on_attach = function()
+--                 vim.keymap.set("n", "<leader><leader>", ":w<cr>:! clang-format -i %<cr><cr>",
+--                         { silent = true, noremap = true })
+--         end
+-- })
 
 vim.lsp.enable('clangd') -- C
 vim.lsp.enable('pylsp')  -- python
 vim.lsp.enable('lua_ls') -- lua
+vim.lsp.config('lua_ls', {
+        settings = {
+                Lua = {
+                        workspace = {
+                                library = vim.api.nvim_get_runtime_file("", true), }
+                }
+        }
+})
 
 vim.diagnostic.config({
-        virtual_text = true,
+        virtual_text = false, -- show with C-w d
         signs = true,
         underline = true,
         update_in_insert = false,
@@ -217,37 +162,15 @@ vim.diagnostic.config({
 
 local builtin = require('telescope.builtin')
 
-vim.keymap.set('n', '<C-p>', builtin.git_files, {})
-vim.keymap.set('n', '<leader>fg', function()
-        builtin.grep_string({ search = vim.fn.input("Grep > ") })
-end)
-vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
 vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
 vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
+vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+
+-- lsp find implementation, definition, type def
 vim.keymap.set('n', '<leader>fi', builtin.lsp_implementations, {})
 vim.keymap.set('n', '<leader>fd', builtin.lsp_definitions, {})
 vim.keymap.set('n', '<leader>ft', builtin.lsp_type_definitions, {})
-vim.keymap.set('n', '<leader>fa', builtin.treesitter, {})
 
-require 'nvim-treesitter.configs'.setup {
-        -- A list of parser names, or "all"
-        ensure_installed = { "vimdoc", "c", "lua", "markdown", "python" },
-
-        -- Install parsers synchronously (only applied to `ensure_installed`)
-        sync_install = true,
-
-        -- Automatically install missing parsers when entering buffer
-        -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
-        auto_install = true,
-
-        highlight = {
-                -- `false` will disable the whole extension
-                enable = true,
-
-                -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
-                -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
-                -- Using this option may slow down your editor, and you may see some duplicate highlights.
-                -- Instead of true it can also be a list of languages
-                additional_vim_regex_highlighting = false,
-        },
-}
+vim.keymap.set('n', '<leader>fs', builtin.git_status, {})
+vim.keymap.set('n', '<leader>m', builtin.man_pages, {})
