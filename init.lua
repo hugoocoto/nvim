5d842fda5effcdf43a82f5887271459e54b3d0d9
Hugo Coto Florez <hugocoto100305@gmail.com>
2025-08-15 00:05:47 +0200

    Using nvim nightly with vim.pack

--- init.lua
+++ init.lua
@@ -1,101 +1,70 @@
-------------------------------------------------------------------------------
--- Lazy installation
--------------------------------------------------------------------------------
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not (vim.uv or vim.loop).fs_stat(lazypath) then
-        local lazyrepo = "https://github.com/folke/lazy.nvim.git"
-        local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
-        if vim.v.shell_error ~= 0 then
-                vim.api.nvim_echo({
-                        { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
-                        { out,                            "WarningMsg" },
-                        { "\nPress any key to exit..." },
-                }, true, {})
-                vim.fn.getchar()
-                os.exit(1)
-        end
-end
-vim.opt.rtp:prepend(lazypath)
-
 -------------------------------------------------------------------------------
 -- Plugins
 -------------------------------------------------------------------------------
-require("lazy").setup({
-        spec = {
-                { "stevearc/oil.nvim" },
-                { "folke/lazy.nvim" },
-                { "wakatime/vim-wakatime" },
-                { "neovim/nvim-lspconfig" },
-                { "williamboman/mason.nvim" },
-                {
-                        'chomosuke/typst-preview.nvim',
-                        ft = 'typst',
-                        version = '1.*',
-                },
-                {
-                        "nvim-treesitter/nvim-treesitter",
-                        lazy = false,
-                        branch = 'master',
-                        build = ":TSUpdate"
-                },
-                {
-                        'saghen/blink.cmp',
-                        version = '1.*',
-                        dependencies = { 'rafamadriz/friendly-snippets' },
-                        opts = { keymap = { preset = 'enter' }, },
-                        opts_extend = { "sources.default" }
-                },
-                { "echasnovski/mini.pick", version = "*" },
-                { "hugoocoto/shoebill",    lazy = false },
-                {
-                        'nvim-telescope/telescope.nvim',
-                        branch = '0.1.x',
-                        dependencies = { 'nvim-lua/plenary.nvim' }
-                },
-                { "nvzone/showkeys", cmd = "ShowkeysToggle" },
+vim.pack.add({
+        "https://github.com/stevearc/oil.nvim",
+        "https://github.com/folke/lazy.nvim",
+        "https://github.com/wakatime/vim-wakatime",
+        "https://github.com/neovim/nvim-lspconfig",
+        "https://github.com/williamboman/mason.nvim",
+        "https://github.com/hugoocoto/shoebill",
+        "https://github.com/nvzone/showkeys",
+        "https://github.com/nvim-treesitter/nvim-treesitter",
+        "https://github.com/nvim-lua/plenary.nvim",
+        "https://github.com/rafamadriz/friendly-snippets",
+        {
+                src = "https://github.com/chomosuke/typst-preview.nvim",
+                version = vim.version.range("1.*"),
         },
-        checker = { enabled = false },
+        {
+                src = "https://github.com/nvim-telescope/telescope.nvim",
+                version = vim.version.range("0.1.*"),
+        },
+        {
+                src = "https://github.com/saghen/blink.cmp",
+                version = vim.version.range("1.*"),
+        },
 })
+
+vim.api.nvim_create_user_command("PackUpdate", function()
+        vim.pack.update()
+end, {})
 
 -------------------------------------------------------------------------------
 -- Options
 -------------------------------------------------------------------------------
--- This is as based as it's written in vimscript
-vim.cmd [[
-let g:did_install_default_menus=1
-let g:loaded_netrwPlugin=0
-set guicursor=""
-set tabstop=8
-set shiftwidth=8
-set softtabstop=-1
-set expandtab
-set smartindent
-set relativenumber
-set nu
-set nowrap
-set nohlsearch
-set incsearch
-set scrolloff=8
-set sidescrolloff=8
-set updatetime=1000
-set conceallevel=0
-set noswapfile
-set nobackup
-set undofile
-set wildignorecase
-set ignorecase
-set smartcase
-set noshowmode
-set noruler
-set noshowcmd
-]]
-
+vim.g.did_install_default_menus = 1
+vim.g.loaded_netrwPlugin = 0
 vim.g.mapleader = " "
-vim.opt.clipboard = 'unnamedplus'
-vim.opt.virtualedit = "block"
-vim.opt.colorcolumn = "80"
-vim.opt.signcolumn = "yes"
-vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
+vim.o.tabstop = 8
+vim.o.shiftwidth = 8
+vim.o.softtabstop = -1
+vim.o.expandtab = true
+vim.o.smartindent = true
+vim.o.relativenumber = true
+vim.o.number = true
+vim.o.wrap = false
+vim.o.scrolloff = 8
+vim.o.sidescrolloff = 8
+vim.o.hlsearch = false
+vim.o.incsearch = true
+vim.o.ignorecase = true
+vim.o.smartcase = true
+vim.o.updatetime = 1000
+vim.o.conceallevel = 0
+vim.o.colorcolumn = "80"
+vim.o.signcolumn = "yes"
+vim.o.guicursor = ""
+vim.o.virtualedit = "block"
+vim.o.swapfile = false
+vim.o.backup = false
+vim.o.undofile = true
+vim.o.undodir = os.getenv("HOME") .. "/.vim/undodir"
+vim.o.wildignorecase = true
+vim.o.showmode = false
+vim.o.ruler = false
+vim.o.showcmd = false
+vim.o.clipboard = "unnamedplus"
 
 -------------------------------------------------------------------------------
 -- Misc stuff
@@ -114,12 +83,13 @@ vim.cmd("colorscheme shoebill-forest")
 -------------------------------------------------------------------------------
 -- Remaps and other stuff
 -------------------------------------------------------------------------------
-vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
-vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
-vim.keymap.set("n", "J", "mzJ`z")
+-- vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+-- vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+vim.keymap.set("x", "<leader>p", [["_dP]])
 
 vim.keymap.set("n", "<leader><leader>", vim.lsp.buf.format)
-vim.keymap.set("n", "gd", vim.lsp.buf.definition) -- it is not default?
+vim.keymap.set("n", "gd", vim.lsp.buf.definition)
 
 vim.keymap.set("n", "<C-k>", "<cmd>cprev<CR>zz")
 vim.keymap.set("n", "<C-j>", "<cmd>cnext<CR>zz")
@@ -130,22 +100,18 @@ vim.keymap.set('n', '<leader>fg', builtin.live_grep)
 vim.keymap.set('n', '<leader>fb', builtin.buffers)
 vim.keymap.set('n', '<leader>e', ":Oil<cr>")
 
--- I stole this from someone and I like it
 vim.keymap.set('n', '<bs>', function()
         vim.diagnostic.config({ virtual_lines = not vim.diagnostic.config().virtual_lines })
-        vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled())
 end)
-vim.keymap.set('n', '<cr>', vim.cmd.write)
-
 
 -------------------------------------------------------------------------------
 -- LSP
 -------------------------------------------------------------------------------
-vim.lsp.enable('clangd')   -- C
-vim.lsp.enable('pylsp')    -- python
-vim.lsp.enable('tinymist') -- typst
-vim.lsp.enable('lua_ls')   -- lua
-vim.lsp.enable('bashls')   -- bash, shell
+vim.lsp.enable('clangd')
+vim.lsp.enable('pylsp')
+vim.lsp.enable('tinymist')
+vim.lsp.enable('lua_ls')
+vim.lsp.enable('bashls')
 
 vim.lsp.config('tinymist', { settings = { formatterMode = 'typstyle' } })
 vim.lsp.config('lua_ls', { -- remove undeclared vim
@@ -161,7 +127,7 @@ vim.lsp.config('lua_ls', { -- remove undeclared vim
 -- Diagnostics
 -------------------------------------------------------------------------------
 vim.diagnostic.config({
-        virtual_text = false, -- show with C-w d
+        virtual_text = false,
         signs = true,
         underline = false,
         update_in_insert = false,
@@ -173,6 +139,7 @@ vim.diagnostic.config({
 -------------------------------------------------------------------------------
 require 'mason'.setup()
 require 'oil'.setup()
+
 require 'nvim-treesitter.configs'.setup {
         ensure_installed = {},
         sync_install = false,
@@ -184,3 +151,34 @@ require 'nvim-treesitter.configs'.setup {
                 additional_vim_regex_highlighting = false,
         },
 }
+
+require 'blink.cmp'.setup {
+        keymap = { preset = 'enter' }
+}
+
+------------------------------------------------------------------------------
+-- Lazy installation
+-------------------------------------------------------------------------------
+-- local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+-- if not (vim.uv or vim.loop).fs_stat(lazypath) then
+--         local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+--         local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+--         if vim.v.shell_error ~= 0 then
+--                 vim.api.nvim_echo({
+--                         { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+--                         { out,                            "WarningMsg" },
+--                         { "\nPress any key to exit..." },
+--                 }, true, {})
+--                 vim.fn.getchar()
+--                 os.exit(1)
+--         end
+-- end
+-- vim.opt.rtp:prepend(lazypath)
+
+-- require("lazy").setup({
+--         spec = {
+--                 {
+--                 },
+--         },
+--         checker = { enabled = false },
+-- })
