c9fff13f998f7b71afbc137838693abdbb9ed697
Hugo Coto Florez <hugocoto100305@gmail.com>
2026-04-23 10:33:19 +0200

    Moving to v0.12 (again) + add more colorschemes

--- init.lua
+++ init.lua
@@ -1,138 +1,52 @@
 -------------------------------------------------------------------------------
--- Plugins
--------------------------------------------------------------------------------
-
-vim.pack.add({
-    "https://github.com/neovim/nvim-lspconfig",
-
-    "https://github.com/stevearc/oil.nvim",
-    "https://github.com/nvim-mini/mini.pick",
-    "https://github.com/nvim-mini/mini.extra",
-    "https://github.com/wakatime/vim-wakatime",
-
-    "https://github.com/rose-pine/neovim",
-    "https://github.com/sainnhe/everforest",
-    "https://github.com/sainnhe/gruvbox-material",
-    "https://github.com/hugoocoto/hforest",
-
-    "https://github.com/chomosuke/typst-preview.nvim",
-})
-
-vim.api.nvim_create_user_command("PackUpdate", function()
-    vim.pack.update()
-end, {})
-
--------------------------------------------------------------------------------
 -- Options
 -------------------------------------------------------------------------------
 
-vim.g.did_install_default_menus = 1
-vim.g.loaded_netrwPlugin = 0
-vim.g.mapleader = " "
-vim.o.tabstop = 4
-vim.o.shiftwidth = 4
-vim.o.softtabstop = -1
-vim.o.expandtab = true
-vim.o.smartindent = true
-vim.o.relativenumber = true
-vim.o.number = true
-vim.o.wrap = false
-vim.o.scrolloff = 4
-vim.o.hlsearch = false
-vim.o.incsearch = true
-vim.o.ignorecase = true
-vim.o.smartcase = true
-vim.o.updatetime = 1000
-vim.o.conceallevel = 0
-vim.o.colorcolumn = "+0"
-vim.o.textwidth = 80
-vim.o.signcolumn = "yes"
-vim.o.guicursor = ""
-vim.o.virtualedit = "block"
-vim.o.swapfile = false
-vim.o.backup = false
-vim.o.undofile = true
-vim.o.undodir = os.getenv("HOME") .. "/.vim/undodir"
-vim.o.wildignorecase = true
-vim.o.showmode = false
-vim.o.ruler = false
-vim.o.showcmd = false
-vim.o.clipboard = "unnamedplus"
-vim.o.termguicolors = true
-vim.opt.spelllang = { "en", "es" }
-vim.o.path = '**'
-vim.o.autocomplete = false
-vim.o.completeopt = 'menu,noselect,menuone,popup'
-vim.o.complete = '.,b,k'
-vim.o.autocomplete = false
-vim.o.pummaxwidth =0
+vim.g.did_install_default_menus = 1 -- avoid stupid menu.vim (saves ~100ms)
+vim.g.loaded_netrwPlugin = 0        -- Disable netrw. 🚮 (comment from justinmk)
+vim.opt.shortmess:append("I")       -- Disable start menu
 
--------------------------------------------------------------------------------
--- Misc stuff
--------------------------------------------------------------------------------
-vim.g.gruvbox_material_background = 'hard'
-vim.g.gruvbox_material_disable_italic_comment = 1
-vim.cmd("colorscheme gruvbox-material")
+vim.g.mapleader = " "               -- leader key (Space)
+vim.o.guicursor = ""                -- use block cursor
+vim.o.tabstop = 4                   -- tab display width
+vim.o.shiftwidth = 4                -- indent width
+vim.o.softtabstop = -1              -- follow shiftwidth
+vim.o.expandtab = true              -- tabs -> spaces
+vim.o.smartindent = true            -- auto-indent new lines
+vim.o.relativenumber = true         -- relative line numbers
+vim.o.number = true                 -- absolute current line number
+vim.o.wrap = false                  -- disable line wrapping
+vim.o.scrolloff = 4                 -- keep context around cursor
+vim.o.hlsearch = false              -- don't persist search highlight
+vim.o.incsearch = true              -- incremental search
+vim.o.ignorecase = true             -- case-insensitive search
+vim.o.smartcase = true              -- smart case when uppercase used
+vim.o.updatetime = 1000             -- slower idle update events
+vim.o.conceallevel = 0              -- show concealed text plainly
+vim.o.lazyredraw = true             -- skip some intermediate redraws
 
--- Do not open pdf with nvim
-vim.api.nvim_create_autocmd("BufReadCmd", {
-    pattern = "*.pdf",
-    callback = function(ev)
-        vim.fn.jobstart({ "zathura", ev.file }, { detach = true })
-        vim.cmd("quit")
-    end,
-})
+vim.o.colorcolumn = "+0"            -- highlight at textwidth
+vim.o.textwidth = 80                -- preferred line width
+vim.o.signcolumn = "yes"            -- always show sign column
 
-vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
-    pattern = { "*.c", "*.h" },
-    callback = function()
-        vim.opt_local.tabstop = 8
-        vim.opt_local.shiftwidth = 8
-        vim.opt_local.softtabstop = -1
-        vim.bo.filetype = "c"
-    end,
-})
+vim.o.swapfile = false              -- disable swap files
+vim.o.backup = false                -- disable backup files
+vim.o.undofile = true               -- persistent undo
 
-vim.api.nvim_create_autocmd({ "BufReadPre", "BufNewFile" }, {
-    pattern = { "*.html", "*.css" },
-    callback = function()
-        vim.opt_local.tabstop = 2
-        vim.opt_local.shiftwidth = 2
-        vim.opt_local.softtabstop = -1
-    end,
-})
+-- vim.o.undodir = os.getenv("HOME") .. "/.vim/undodir" -- undo file directory
+vim.cmd("set path+=/usr/lib/gcc/**/include,**")
 
+vim.o.clipboard = "unnamedplus"    -- use system clipboard
+vim.o.ruler = false                -- hide ruler
+vim.o.showcmd = false              -- hide partial command display
+vim.o.showmode = false             -- hide default mode text
+vim.opt.spelllang = { "en", "es" } -- spellcheck languages
 
--------------------------------------------------------------------------------
--- LSP
--------------------------------------------------------------------------------
-
-vim.lsp.config('tinymist', {
-    settings = { formatterMode = 'typstyle' }
-})
-
-vim.lsp.config('lua_ls', {
-    settings = {
-        Lua = {
-            workspace = {
-                library = vim.api.nvim_get_runtime_file("", true), }
-        }
-    }
-})
-
-vim.lsp.enable({
-    'clangd',
-    'pylsp',
-    'tinymist',
-    'lua_ls',
-    'bashls',
-    'html',
-    'cssls',
-    'dockerls',
-    'hls',
-    'jdtls',
-    'rust_analyzer',
-})
+-- completion
+vim.o.autocomplete = false                        -- manual completion only
+vim.o.complete = '.,b,k'                          -- completion sources
+vim.o.completeopt = 'menu,noselect,menuone,popup' -- popup completion behavior
+vim.o.pummaxwidth = 0                             -- no popup width cap
 
 -------------------------------------------------------------------------------
 -- Diagnostics
@@ -144,6 +58,87 @@ vim.diagnostic.config({
     update_in_insert = false,
     severity_sort = true,
 })
+
+-------------------------------------------------------------------------------
+-- Remaps and other stuff
+-------------------------------------------------------------------------------
+vim.keymap.set("n", "<leader><leader>", vim.lsp.buf.format)
+vim.keymap.set("v", "<leader>p", [["_dP]])
+vim.keymap.set("n", "<C-k>", "<cmd>cprev<CR>zz")
+vim.keymap.set("n", "<C-j>", "<cmd>cnext<CR>zz")
+vim.keymap.set("c", "wq", "x")
+vim.keymap.set("x", "<leader>e", "y:echo <C-r>\"<cr>gv")
+vim.keymap.set("n", "<leader>c", "1z=")
+vim.keymap.set('n', '<bs>', function()
+    vim.diagnostic.config({ virtual_lines = not vim.diagnostic.config().virtual_lines })
+end)
+
+vim.keymap.set('n', '<leader>e', ":Oil<cr>")
+vim.keymap.set('t', '<Esc>', [[<C-\><C-N>]])
+
+-- Write a typst image func for the last screen capture
+vim.keymap.set('n', '<leader>ls', function()
+    -- configuration:
+    local source_dir = os.getenv("HOME") .. "/ScreenCaptures"
+    local dest_dir = "images"
+
+    local handle = io.popen("ls -t " .. source_dir .. " | head -n 1")
+    if (handle) then
+        local filename = handle:read("*a")
+        handle:close()
+        filename = string.gsub(filename, "%s+", "")
+        os.execute("mkdir -p " .. dest_dir)
+        os.execute(string.format("cp '%s/%s' '%s/'", source_dir, filename, dest_dir))
+        vim.api.nvim_put({ "#image(\"images/" .. filename .. "\")" }, 'c', false, true)
+    end
+end)
+
+-- bold and italic for typst
+vim.keymap.set('x', '<C-b>', [[c**<esc>P]])
+vim.keymap.set('x', '<C-_>', [[c__<esc>P]])
+
+-- pickers
+vim.keymap.set('n', '<Leader>f/', '<Cmd>Pick history scope="/"<CR>', { desc = '"/" history' })
+vim.keymap.set('n', '<Leader>f:', '<Cmd>Pick history scope=":"<CR>', { desc = '":" history' })
+vim.keymap.set('n', '<Leader>fC', '<Cmd>Pick git_commits path="%"<CR>', { desc = 'Commits (buf)' })
+vim.keymap.set('n', '<Leader>fD', '<Cmd>Pick diagnostic scope="current"<CR>', { desc = 'Diagnostic buffer' })
+vim.keymap.set('n', '<Leader>fG', '<Cmd>Pick grep pattern="<cword>"<CR>', { desc = 'Grep current word' })
+vim.keymap.set('n', '<Leader>fa', '<Cmd>Pick git_hunks scope="staged"<CR>', { desc = 'Added hunks (all)' })
+vim.keymap.set('n', '<Leader>fb', '<Cmd>Pick buffers<CR>', { desc = 'Buffers' })
+vim.keymap.set('n', '<Leader>fc', '<Cmd>Pick git_commits<CR>', { desc = 'Commits (all)' })
+vim.keymap.set('n', '<Leader>fd', '<Cmd>Pick diagnostic scope="all"<CR>', { desc = 'Diagnostic workspace' })
+vim.keymap.set('n', '<Leader>ff', '<Cmd>Pick files<CR>', { desc = 'Files' })
+vim.keymap.set('n', '<Leader>fg', '<Cmd>Pick grep_live<CR>', { desc = 'Grep live' })
+vim.keymap.set('n', '<Leader>fh', '<Cmd>Pick help<CR>', { desc = 'Help tags' })
+vim.keymap.set('n', '<Leader>fr', '<Cmd>Pick resume<CR>', { desc = 'Resume' })
+
+-------------------------------------------------------------------------------
+-- Plugins
+-------------------------------------------------------------------------------
+
+vim.pack.add({
+    "https://github.com/neovim/nvim-lspconfig",
+    "https://github.com/chomosuke/typst-preview.nvim",
+
+    "https://github.com/stevearc/oil.nvim",
+    "https://github.com/nvim-mini/mini.pick",
+    "https://github.com/nvim-mini/mini.extra",
+    "https://github.com/wakatime/vim-wakatime",
+
+    "https://github.com/cocopon/iceberg.vim",
+    "https://github.com/hugoocoto/hforest",
+    "https://github.com/morhetz/gruvbox",
+    "https://github.com/nanotech/jellybeans.vim",
+    "https://github.com/rebelot/kanagawa.nvim",
+    "https://github.com/rose-pine/neovim",
+    "https://github.com/sainnhe/everforest",
+    "https://github.com/sainnhe/gruvbox-material",
+    "https://github.com/vague-theme/vague.nvim",
+})
+
+vim.api.nvim_create_user_command("PackUpdate", function()
+    vim.pack.update()
+end, {})
 
 -------------------------------------------------------------------------------
 -- Plugin setup
@@ -207,57 +202,70 @@ require 'typst-preview'.setup {
 
 require 'mini.extra'.setup()
 require 'mini.pick'.setup()
+require 'oil'.setup()
 
 -------------------------------------------------------------------------------
--- Remaps and other stuff
+-- Misc stuff
 -------------------------------------------------------------------------------
-vim.keymap.set("n", "<leader><leader>", vim.lsp.buf.format)
-vim.keymap.set("v", "<leader>p", [["_dP]])
-vim.keymap.set("n", "<C-k>", "<cmd>cprev<CR>zz")
-vim.keymap.set("n", "<C-j>", "<cmd>cnext<CR>zz")
-vim.keymap.set("c", "wq", "x")
-vim.keymap.set("v", "<leader>e", "y:echo <C-r>\"<cr>gv")
-vim.keymap.set("n", "<leader>c", "1z=")
-vim.keymap.set('n', '<bs>', function()
-    vim.diagnostic.config({ virtual_lines = not vim.diagnostic.config().virtual_lines })
-end)
-vim.keymap.set('n', '<leader>e', ":Oil<cr>")
+vim.g.gruvbox_material_background = 'hard'
+vim.g.gruvbox_material_disable_italic_comment = 1
+vim.cmd("colorscheme gruvbox-material")
 
--- Write a typst image func for the last screen capture
-vim.keymap.set('n', '<leader>ls', function()
-    -- configuration:
-    local source_dir = os.getenv("HOME") .. "/ScreenCaptures"
-    local dest_dir = "images"
+-- Do not open pdf with nvim
+vim.api.nvim_create_autocmd("BufReadCmd", {
+    pattern = "*.pdf",
+    callback = function(ev)
+        vim.fn.jobstart({ "zathura", ev.file }, { detach = true })
+        vim.cmd("quit")
+    end,
+})
 
-    local handle = io.popen("ls -t " .. source_dir .. " | head -n 1")
-    if (handle) then
-        local filename = handle:read("*a")
-        handle:close()
-        filename = string.gsub(filename, "%s+", "")
-        os.execute("mkdir -p " .. dest_dir)
-        os.execute(string.format("cp '%s/%s' '%s/'", source_dir, filename, dest_dir))
-        vim.api.nvim_put({ "#image(\"images/" .. filename .. "\")" }, 'c', false, true)
-    end
-end)
+vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
+    pattern = { "*.c", "*.h" },
+    callback = function()
+        vim.opt_local.tabstop = 8
+        vim.opt_local.shiftwidth = 8
+        vim.opt_local.softtabstop = -1
+        vim.bo.filetype = "c"
+    end,
+})
 
--- bold and italic for typst
-vim.keymap.set('x', '<C-b>', [[c**<esc>P]])
-vim.keymap.set('x', '<C-_>', [[c__<esc>P]])
+vim.api.nvim_create_autocmd({ "BufReadPre", "BufNewFile" }, {
+    pattern = { "*.html", "*.css" },
+    callback = function()
+        vim.opt_local.tabstop = 2
+        vim.opt_local.shiftwidth = 2
+        vim.opt_local.softtabstop = -1
+    end,
+})
 
-local nmap_leader = function(suffix, rhs, desc)
-    vim.keymap.set('n', '<Leader>' .. suffix, rhs, { desc = desc })
-end
+-------------------------------------------------------------------------------
+-- LSP
+-------------------------------------------------------------------------------
 
-nmap_leader('f/', '<Cmd>Pick history scope="/"<CR>', '"/" history')
-nmap_leader('f:', '<Cmd>Pick history scope=":"<CR>', '":" history')
-nmap_leader('fC', '<Cmd>Pick git_commits path="%"<CR>', 'Commits (buf)')
-nmap_leader('fD', '<Cmd>Pick diagnostic scope="current"<CR>', 'Diagnostic buffer')
-nmap_leader('fG', '<Cmd>Pick grep pattern="<cword>"<CR>', 'Grep current word')
-nmap_leader('fa', '<Cmd>Pick git_hunks scope="staged"<CR>', 'Added hunks (all)')
-nmap_leader('fb', '<Cmd>Pick buffers<CR>', 'Buffers')
-nmap_leader('fc', '<Cmd>Pick git_commits<CR>', 'Commits (all)')
-nmap_leader('fd', '<Cmd>Pick diagnostic scope="all"<CR>', 'Diagnostic workspace')
-nmap_leader('ff', '<Cmd>Pick files<CR>', 'Files')
-nmap_leader('fg', '<Cmd>Pick grep_live<CR>', 'Grep live')
-nmap_leader('fh', '<Cmd>Pick help<CR>', 'Help tags')
-nmap_leader('fr', '<Cmd>Pick resume<CR>', 'Resume')
+vim.lsp.config('tinymist', {
+    settings = { formatterMode = 'typstyle' }
+})
+
+vim.lsp.config('lua_ls', {
+    settings = {
+        Lua = {
+            workspace = {
+                library = vim.api.nvim_get_runtime_file("", true), }
+        }
+    }
+})
+
+vim.lsp.enable({
+    'clangd',
+    'pylsp',
+    'tinymist',
+    'lua_ls',
+    'bashls',
+    'html',
+    'cssls',
+    'dockerls',
+    'hls',
+    'jdtls',
+    'rust_analyzer',
+})
